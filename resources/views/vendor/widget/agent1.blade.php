207ae90aa83c9e8aa20fef0eeb3d2d9866076e4a
on-moebius-strip <jimoebius@gmail.com>
2015-10-17 17:26:03 +0300

    added agents

--- resources/views/vendor/widget/agent1.blade.php
+++ resources/views/vendor/widget/agent1.blade.php
@@ -1,9 +1,9 @@
-<div class="agent">
+<div class="agent1">
 	<a href="http://mariusn.com/themes/reales/profile.html" class="agent-avatar">
-		<img src="avatar-3.png" alt="Agent">
+		<img src="avatar-5.png" alt="Giannakis Dimitrios">
 		<div class="ring"></div>
 	</a>
-	<div class="agent-name osLight">Συνεργάτης</div>
+	<div class="agent-name osLight">Γιαννάκης Δημήτριος</div>
 	<!-- <div class="agent-rating">
 		<span class="fa fa-star text-yellow"></span>
 		<span class="fa fa-star text-yellow"></span>
