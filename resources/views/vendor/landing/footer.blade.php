87466fafbf681501796756e285f3b1d0c5ef25d2
on-moebius-strip <jimoebius@gmail.com>
2015-10-17 14:57:29 +0300

    commiting enterprise info and more

--- resources/views/vendor/landing/footer.blade.php
+++ resources/views/vendor/landing/footer.blade.php
@@ -3,22 +3,22 @@
     <div class="home-wrapper">
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-                <div class="osLight footer-header">Company</div>
+                <div class="osLight footer-header">Εταιρία</div>
                 <ul class="footer-nav pb20">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Press</a></li>
+                    <li><a href="#">Πληροφορίες</a></li>
+                    <li><a href="#">Εργασία</a></li>
+                    <li><a href="#">Ειδήσεις</a></li>
                     <li><a href="http://mariusn.com/themes/reales/blog.html">Blog</a></li>
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">Policies</a></li>
+                    <li><a href="#">Βοήθεια</a></li>
+                    <li><a href="#">Πολιτικές Εταιρίας</a></li>
                     <li><a href="#">Terms &amp; Privacy</a></li>
                 </ul>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-                <div class="osLight footer-header">Discover</div>
+                <div class="osLight footer-header">Ανακαλύψτε</div>
                 <ul class="footer-nav pb20">
-                    <li><a href="#">Become a Member</a></li>
-                    <li><a href="#">Properties List</a></li>
+                    <li><a href="#">Γίνετε Μέλος</a></li>
+                    <li><a href="#">Λίστα Προορισμών</a></li>
                     <li><a href="#">Sign In</a></li>
                     <li><a href="#">Widgets</a></li>
                     <li><a href="#">Components</a></li>
@@ -27,19 +27,19 @@
                 </ul>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <div class="osLight footer-header">Get in Touch</div>
+                <div class="osLight footer-header">Επικοινωνία</div>
                 <ul class="footer-nav pb20">
-                    <li class="footer-phone"><span class="fa fa-phone"></span> 800 234 67 89</li>
+                    <li class="footer-phone"><span class="fa fa-phone"></span> 6978955287</li>
                     <li class="footer-address osLight">
-                        <p>516 Green St</p>
-                        <p>San Francisco, CA 94133</p>
-                        <p>United States</p>
+                        <p>Πατριαρχου Ιερεμίου 5</p>
+                        <p>Αθήνα, Αττική 114-71</p>
+                        <p>Ελλάδα</p>
                     </li>
                     <li><a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-white"><span class="fa fa-facebook"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-white"><span class="fa fa-twitter"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-white"><span class="fa fa-google-plus"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-white"><span class="fa fa-linkedin"></span></a> </li>
                 </ul>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <div class="osLight footer-header">Subscribe to Our Newsletter</div>
+                <div class="osLight footer-header">Μάθετε πρώτοι τα νέα μας!</div>
                 <form role="form">
                     <div class="form-group">
                         <input class="form-control" placeholder="Email Address" type="email">
