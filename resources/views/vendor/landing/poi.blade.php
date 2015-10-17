e49ac40a7e464b7215866c0893c419dc5d2285e1
on-moebius-strip <jimoebius@gmail.com>
2015-10-17 21:20:05 +0300

    dunno trying

--- resources/views/vendor/landing/poi.blade.php
+++ resources/views/vendor/landing/poi.blade.php
@@ -1,4 +1,5 @@
 <h2 class="osLight">Δημοφιλείς Προορισμοί</h2>
+<!-- εμφανίζει προορισμούς που έχει ξαναδεί ο χρήστης ή random αν δεν έχει preferences -->
 <div class="row pb40">
 	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
 		@include('vendor.widget.poi-1')
@@ -12,6 +13,7 @@
 	
 </div>
 <h2 class="osLight">Προτεινόμενοι Προορισμοί</h2>
+<!-- εμφανίζει προορισμούς με παρόμοια αξιοθέατα/δραστηριότητες με αυτές που έχει ο χρήστης πάνω -->
 <div class="row ">
 	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
 		@include('vendor.widget.poi-1')
