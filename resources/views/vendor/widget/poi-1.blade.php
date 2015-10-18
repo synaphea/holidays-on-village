faf71dea2d826f498fad13de43945b56040edc1d
on-moebius-strip <jimoebius@gmail.com>
2015-10-18 11:40:48 +0300

    Poi mod

--- resources/views/vendor/widget/poi-1.blade.php
+++ resources/views/vendor/widget/poi-1.blade.php
@@ -4,16 +4,13 @@
         <img height="221" src="https://scontent-fra3-1.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/11248726_1463439890629954_875741758_n.jpg" alt="image">
     <!-- εφανίζει μέσο όρο τιμών απο fourscuare -->
         <div class="priceCap"><span>$1,930,000</span></div>
-    <!-- περιτο -->
-        <div class="figType">FOR SALE</div>
         <div class="figCap">
     <!-- εμφανίζει το όνομα του προορισμού / αξιοθέατου -->
-            <h3>Modern Residence in New York</h3>
+            <h3>Εξωτικός Προορισμός</h3>
     <!-- εμφανίζει την διεύθυνση ή νομό/περιοχή του προορισμού -->
-            <div class="address">39 Remsen St, Brooklyn, NY 11201, USA</div>
+            <div class="address">Λεκανοπέδιο Αττικής</div>
             <div class="feat feat-1"><span class="fa fa-moon-o"></span> 3</div>
             <div class="feat feat-2"><span class="icon-drop"></span> 2</div>
             <div class="feat feat-3"><span class="icon-frame"></span> 3430 Sq Ft</div>
         </div>
     </div>
-</a>
