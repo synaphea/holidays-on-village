3e420773183d217df2e17f1fb1b00dfd8f7b5903
on-moebius-strip <jimoebius@gmail.com>
2015-10-18 11:10:45 +0300

    MAXIMISED LANDING PAGE

---
+++ resources/views/vendor/widget/agent4.blade.php
@@ -0,0 +1,17 @@
+<div class="agent4">
+	<a href="http://mariusn.com/themes/reales/profile.html" class="agent-avatar">
+		<img src="avatar-8.png" alt="Theofilis Giorgos">
+		<div class="ring"></div>
+	</a>
+	<div class="agent-name osLight">Θεωφίλης Γιώργος</div>
+	<!-- <div class="agent-rating">
+		<span class="fa fa-star text-yellow"></span>
+		<span class="fa fa-star text-yellow"></span>
+		<span class="fa fa-star text-yellow"></span>
+		<span class="fa fa-star text-yellow"></span>
+		<span class="fa fa-star text-yellow"></span>
+	</div> -->
+	<div class="agent-contact">
+		<a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-green"><span class="fa fa-envelope-o"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-facebook"><span class="fa fa-facebook"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-twitter"><span class="fa fa-twitter"></span></a> <a href="#" class="btn btn-sm btn-icon btn-round btn-o btn-google"><span class="fa fa-google-plus"></span></a>
+	</div>
+</div>
