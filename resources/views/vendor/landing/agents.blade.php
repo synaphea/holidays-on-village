8d65db6d614a71dc76c627024903a304d857253f
billba <bill_vasilopoulos@hotmail.com>
2015-10-18 16:49:38 +0300

    landing team view

--- resources/views/vendor/landing/agents.blade.php
+++ resources/views/vendor/landing/agents.blade.php
@@ -1,4 +1,4 @@
-<h2 class="osLight">Συνεργάτες</h2>
+<h2 class="osLight">Η ΟΜΑΔΑ ΜΑΣ</h2>
 <div class="row pb40">
 	<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
 		@include('vendor.widget.agent1')
--- resources/views/vendor/landing/testimonials.blade.php
+++ resources/views/vendor/landing/testimonials.blade.php
@@ -1,8 +1,11 @@
-<h2 class="osLight">Εμπειρίες Χρήσης</h2>
+<h2 class="osLight">Εμπειρίες Ανάπτυξης</h2>
 <div id="home-testimonials" class="carousel slide carousel-wb mb20" data-ride="carousel">
 	<ol class="carousel-indicators">
 		<li data-target="#home-testimonials" data-slide-to="0" class="active"></li>
 		<li data-target="#home-testimonials" data-slide-to="1" class=""></li>
+		<li data-target="#home-testimonials" data-slide-to="2" class=""></li>
+		<li data-target="#home-testimonials" data-slide-to="3" class=""></li>
+		<li data-target="#home-testimonials" data-slide-to="4" class=""></li>
 	</ol>
 	<div class="carousel-inner">
 		<div class="item active">
@@ -22,10 +25,24 @@
 			</div>
 		</div>
 		<div class="item">
-			<img src="avatar-7.png" class="home-testim-avatar" alt="Vasilopoulos Vasilis">
+			<img src="avatar-7.png" class="home-testim-avatar" alt="Konstantinos Kostis">
 			<div class="home-testim">
-				<div class="home-testim-text">Άσπρος γάτος, μαύρος γάτος... Σημασία έχει να πιάνει ποντίκια.</div>
-				<div class="home-testim-name">t</div>
+				<div class="home-testim-text">Αριθμός διψήφιος πολίτης υποψήφιος</div>
+				<div class="home-testim-name">Κωνσταντίνος Κωστής</div>
+			</div>
+		</div>
+		<div class="item">
+			<img src="avatar-8.png" class="home-testim-avatar" alt="Theofilis Georgios">
+			<div class="home-testim">
+				<div class="home-testim-text">Ἓν οἶδα ὅτι οὐδὲν οἶδα</div>
+				<div class="home-testim-name">Θεοφίλης Γεώργιος</div>
+			</div>
+		</div>
+		<div class="item">
+			<img src="avatar-9.png" class="home-testim-avatar" alt="Kokkinos Panagiotis">
+			<div class="home-testim">
+				<div class="home-testim-text">Στις πιο ωραίες θάλασσες δεν έχουμε ακόμα ταξιδέψει</div>
+				<div class="home-testim-name">Κόκκινος Παναγιώτης</div>
 			</div>
 		</div>
 	</div>
