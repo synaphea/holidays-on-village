187f436140db27152cbd2ec701b5bb4348049ece
George Theofilis <theofilis.g@gmail.com>
2015-10-18 11:48:47 +0300

    Update header.blade.php

--- resources/views/vendor/dashboard/header.blade.php
+++ resources/views/vendor/dashboard/header.blade.php
@@ -34,66 +34,6 @@
         </div>
     </div>
 
-    <div class="headerNotifyWraper">
-        <a href="#" class="headerNotify dropdown-toggle" data-toggle="dropdown">
-            <span class="notifyIcon icon-bell"></span>
-            <span class="counter">5</span>
-        </a>
-        <div class="dropdown-menu pull-right notifyMenu" role="menu">
-            <div class="notifyHeader">
-                <span>Notifications</span>
-                <a href="#" class="notifySettings icon-settings"></a>
-                <div class="clearfix"></div>
-            </div>
-            <ul class="notifyList">
-                <li>
-                    <a href="#">
-                        <img class="avatar pull-left" src="{{ Auth::user()->social->avatar }}" alt="avatar">
-                        <div class="pulse border-grey"></div>
-                        <div class="notify pull-left">
-                            <div class="notifyName">Sed ut perspiciatis unde</div>
-                            <div class="notifyTime">5 minutes ago</div>
-                        </div>
-                        <div class="clearfix"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="notifyRound notifyRound-red fa fa-envelope-o"></div>
-                        <div class="pulse border-red"></div>
-                        <div class="notify pull-left">
-                            <div class="notifyName">Lorem Ipsum is simply dummy text</div>
-                            <div class="notifyTime">20 minutes ago</div>
-                        </div>
-                        <div class="clearfix"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="notifyRound notifyRound-yellow fa fa-heart-o"></div>
-                        <div class="pulse border-yellow"></div>
-                        <div class="notify pull-left">
-                            <div class="notifyName">It is a long established fact</div>
-                            <div class="notifyTime">2 hours ago</div>
-                        </div>
-                        <div class="clearfix"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="notifyRound notifyRound-magenta fa fa-paper-plane-o"></div>
-                        <div class="pulse border-magenta"></div>
-                        <div class="notify pull-left">
-                            <div class="notifyName">There are many variations</div>
-                            <div class="notifyTime">1 day ago</div>
-                        </div>
-                        <div class="clearfix"></div>
-                    </a>
-                </li>
-            </ul>
-            <a href="#" class="notifyAll">See All</a>
-        </div>
-    </div>
     <a href="#" class="mapHandler"><span class="icon-map"></span></a>
     <div class="clearfix"></div>
 </div>
